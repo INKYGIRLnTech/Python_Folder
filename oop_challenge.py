11fb08e7c307c20de462db657d4c61faeec0c711
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-10 14:33:20 -0500

    created a classes and instances challenge

---
+++ oop_challenge.py
@@ -0,0 +1,24 @@
+# Python Object-Oriented Programming
+# Classes and Instances
+
+class Apprentice:
+
+    def __init__(self, first, last, language):
+        self.first = first
+        self.last = last
+        self.language = language
+        self.cohort = first + ' ' + last + ':' + language
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+
+app_1 = Apprentice('Meagan', 'Hollingsworth', 'Python')
+app_2 = Apprentice('Daniel', 'Guerrero', 'Go')
+app_3 = Apprentice('Aaron', 'Sanchez', 'Go')
+
+print(app_1.language)
+print(app_2.language)
+
+print(Apprentice.fullname(app_3))
