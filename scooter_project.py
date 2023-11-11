c9115a79b8b5bb467df3ee7e306ebb03778db4ce
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-10 21:37:44 -0500

    created the rent scooter method

--- scooter_project.py
+++ scooter_project.py
@@ -71,3 +71,77 @@ except Exception as e:
 # Logout a user
 user2.logout()
 print(f"User {user2.username} logged out")
+
+
+class ScooterApp:
+    def __init__(self):
+        self.stations = {
+            'Crotona Park Station':[],
+            'Boston Rd Station':[],
+            'Mott Haven':[],
+            'TimeSquare Station':[],
+            'Harlem River Station':[],
+            'Bronx River Station':[],
+            'Yankees Stadium':[]  
+        }
+
+        self.registered_users = {}
+
+    def registered_user(self, username, password, age):
+        if age < 18:
+            raise Exception("Too young to register")
+        elif username in self.registered_users:
+            raise Exception("Already registered")
+        
+        user = User(username, password, age)
+        self.registered_uses[username] = user
+        print(f"User {username} is registered")
+        return user
+    
+    def login_user(self, username, password):
+        user = self.registered_users.get(username)
+        if not user:
+            raise Exception("Username or password is incorrect")
+        
+        try:
+            user.login(password)
+            print(f"User {username} is logged in")
+        except Exception as e:
+            raise Exception("Username or password is incorrect")
+        
+    
+    def logout_user(self, username):
+        user = self.registered_users.get(username)
+        if not user or not user.logged_in:
+            raise Exception("No such user is logged in")
+        
+        user.logout()
+        print(f"User {username} is logged out")
+
+    
+    def create_scooter(self, station):
+        if station not in self.stations:
+            raise Exception('No such station')
+        
+        scooter = Scooter(station)
+        self.station[station].append(scooter)
+        scooter.station = station
+        print(f"Created new scooter at {station}")
+        return scooter 
+    
+    def dock_scooter(self, scooter, station):
+        if station not in self.stations:
+            raise Exception("No such station")
+        if scooter in self.stations[station]:
+            raise Exception("Scooter already at station")
+        
+        self.stations[station].append(scooter)
+        scooter.dock(station)
+        print(f"Scooter is docked at {station}")
+
+    
+    def rent_scooter(self, scooter, user):
+        if not scooter.station:
+            raise Exception("Scooter already rented")
+        elif scooter.station not in self.stations:
+            raise Exception("No such station")
